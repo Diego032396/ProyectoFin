6980e14fa18b7ae70d3d6d359a0f1c70aeb03647
Diego032396 <diegojosuegalvez@gmail.com>
2024-05-15 16:46:18 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoFin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.vLogin();
+        }
+    }
+}
---
+++ Views/vLogin.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProyectoFin.Views;
+
+public partial class vLogin : ContentPage
+{
+	public vLogin()
+	{
+		InitializeComponent();
+	}
+}
