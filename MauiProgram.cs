c338fe332d9b02b95f0dcc2869f340772c2100b7
Diego032396 <diegojosuegalvez@gmail.com>
2024-05-17 11:55:58 -0500

    login4

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,6 +15,7 @@ namespace ProyectoFin
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("Lato-Bold.ttf", "bold");
                     fonts.AddFont("Lato-Regular.ttf", "medium");
+                    fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "AwesomeSolid");
                 });
 
 #if DEBUG
