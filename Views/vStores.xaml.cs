dab7f0b7a6e32632c60375697d5689620dac29f8
Diego032396 <diegojosuegalvez@gmail.com>
2024-05-18 16:34:00 -0500

    camara1

--- Views/vStores.xaml.cs
+++ Views/vStores.xaml.cs
@@ -1,9 +1,16 @@
+
+using System.Reflection.PortableExecutable;
+
 namespace ProyectoFin.Views;
 
 public partial class vStores : ContentPage
 {
+	
 	public vStores()
 	{
 		InitializeComponent();
+		
 	}
+	
+	
 }
