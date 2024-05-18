54cd9f0dabdc2f4e9d1f04a0b0291eced1c47678
Diego032396 <diegojosuegalvez@gmail.com>
2024-05-18 10:40:45 -0500

    Store

--- Views/vLogin.xaml.cs
+++ Views/vLogin.xaml.cs
@@ -6,4 +6,9 @@ public partial class vLogin : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnLogin_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new Views.vStores());
+    }
 }
---
+++ Views/vStores.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProyectoFin.Views;
+
+public partial class vStores : ContentPage
+{
+	public vStores()
+	{
+		InitializeComponent();
+	}
+}
